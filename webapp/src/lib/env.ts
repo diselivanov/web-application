b244523a72753b33216c7f1a7eb63abc6869a943
Danya <dselivanov143@mail.ru.com>
2025-10-02 02:10:41 +0300

    Initial

---
+++ webapp/src/lib/env.ts
@@ -0,0 +1,14 @@
+import { zEnvHost, zEnvNonemptyTrimmed } from '@ideanick/shared/src/zod'
+import { z } from 'zod'
+
+export const zEnv = z.object({
+  NODE_ENV: z.enum(['development', 'production']),
+  HOST_ENV: zEnvHost,
+  VITE_BACKEND_TRPC_URL: zEnvNonemptyTrimmed,
+  VITE_WEBAPP_URL: zEnvNonemptyTrimmed,
+  VITE_CLOUDINARY_CLOUD_NAME: zEnvNonemptyTrimmed,
+})
+
+const envFromBackend = (window as any).webappEnvFromBackend
+// eslint-disable-next-line node/no-process-env
+export const env = zEnv.parse(envFromBackend?.replaceMeWithPublicEnv ? process.env : envFromBackend)
